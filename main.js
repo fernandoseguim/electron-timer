6c1a95dce1e112f951cab937ef5303ce728a2234
Fernando Seguim <fernando.seguim@gmail.com>
2018-04-13 15:05:07 -0300

    create a first window, hello world

---
+++ main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require("electron");
+
+app.on('ready', () => {
+    console.log('Starting application...');
+
+    let mainWindow = new BrowserWindow({
+        width: 600,
+        height: 400
+    });
+
+    mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+});
+
+
+app.on('window-all-closed', () => {
+    app.quit();
+});
